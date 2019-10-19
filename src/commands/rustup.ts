6e5431adab9d19a3e79bd8f5a0a8f98f033fb674
svartalf <self@svartalf.info>
2019-10-19 12:49:36 +0300

    Fixing some rustup installation quirks from within the Docker containers.
    Adding `+x` bit for the downloaded init script.
    Passing `-y` flag for the init script in order to skip any prompts

--- src/commands/rustup.ts
+++ src/commands/rustup.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from 'fs';
 import * as path from 'path';
 import * as process from 'process';
 
@@ -45,13 +46,26 @@ export class RustUp {
     }
 
     public static async install(): Promise<RustUp> {
-        const args = ['--default-toolchain', 'none'];
+        const args = [
+            '--default-toolchain',
+            'none',
+            '-y', // No need for the prompts (hard error from within the Docker containers)
+        ];
 
         switch (process.platform) {
             case 'darwin':
             case 'linux': {
                 // eslint-disable-line prettier/prettier
                 const rustupSh = await tc.downloadTool('https://sh.rustup.rs');
+
+                // While the `rustup-init.sh` is properly executed as is,
+                // when Action is running on the VM itself,
+                // it fails with `EACCES` when called in the Docker container.
+                // Adding the execution bit manually just in case.
+                // See: https://github.com/actions-rs/toolchain/pull/19#issuecomment-543358693
+                core.debug(`Executing chmod 755 on the ${rustupSh}`);
+                await fs.chmod(rustupSh, 0o755);
+
                 await exec.exec(rustupSh, args);
                 break;
             }
